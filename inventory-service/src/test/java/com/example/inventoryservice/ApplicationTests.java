ce526c94ed5e2f6d8f604299f3f93656d8a5059f
heitor <heitorocosta@gmail.com>
2026-03-17 20:17:45 -0300

    fix: adicionando pom e arrumando estrutura do spring initializr

--- inventory-service/src/test/java/com/example/inventoryservice/ApplicationTests.java
+++ inventory-service/src/test/java/com/example/inventoryservice/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.orderservice;
+package com.example.inventoryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderServiceApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
